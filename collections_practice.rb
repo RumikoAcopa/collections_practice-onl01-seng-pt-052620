62db85b06714bf84fd45d5de3d3e93f1262780a9
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-07 18:32:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,3 +51,14 @@ def sum_array(array)
   sum
 end
       
+sum_array([11,4,7,8,9,100,134])
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word = "s"
+    end
+  end
+end    
