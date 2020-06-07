5515813364075da780ba8ac47a695c157a588a5e
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-07 18:25:56 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,13 +3,32 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  array.sort do |a, b, c|
-  if a == c
-    0
-  elsif c > b
-    1
-  elsif b < c
-    -1
+  array.sort do |left, right|
+    right <=> left
   end
 end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
 end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+swap_elements(["blake", "ashley", "scott"])
+
+def reverse_array(array)
+  array.reverse
+end
+
+reverse_array(["blake", "ashley", "scott"])
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end  
